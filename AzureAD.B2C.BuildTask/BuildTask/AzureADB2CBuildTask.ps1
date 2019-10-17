1a022b3080adc0395425cbc87e88e781e16124c6
Manish Tiwari <Manish@cloudthing.com>
2019-10-17 12:10:41 +0530

    Fix json issue and minor changes

--- AzureAD.B2C.BuildTask/BuildTask/AzureADB2CBuildTask.ps1
+++ AzureAD.B2C.BuildTask/BuildTask/AzureADB2CBuildTask.ps1
@@ -6,18 +6,19 @@ param()
 Trace-VstsEnteringInvocation $MyInvocation
 
 # Set the working directory.
-    $DirectoryPath = Get-VstsInput -Name directoryPath -Require
-    $B2CDomain = Get-VstsInput -Name b2cDomain -Require
-    $ClientId = Get-VstsInput -Name clientid -Require
-	$ClientSecret = Get-VstsInput -Name clientsecret -Require
+$DirectoryPath = Get-VstsInput -Name directoryPath -Require
+$B2CDomain = Get-VstsInput -Name b2cDomain -Require
+$ClientId = Get-VstsInput -Name clientid -Require
+$ClientSecret = Get-VstsInput -Name clientsecret -Require
+$JSON = Get-VstsInput -Name json -Require
 try {
     Import-Module .\AzureAD.B2C.BuildTask.dll   
 
-    New-CustomizeAndUploadPolicies -directoryPath "$DirectoryPath"  -b2CDomain "$B2CDomain" -clientId "$ClientId" -clientSecret $ClientSecret -Verbose
+    New-CustomizeAndUploadPolicies -directoryPath "$DirectoryPath"  -b2CDomain "$B2CDomain" -clientId "$ClientId" -clientSecret "$ClientSecret" -JSON "$Json"  -Verbose
 }
 catch{
     Write-Host $_.Exception.Message; 
 }
- finally {
+finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
