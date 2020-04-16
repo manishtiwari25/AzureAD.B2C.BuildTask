bf7e5c1045a43903e9108c44fa83d2a7d0f4c539
Manish Tiwari <manish.ertiwari@outlook.com>
2020-04-16 11:05:13 +0530

    merge

---
+++ AzureAD.B2C.BuildTask/Models/OdataModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AzureAD.B2C.BuildTask.Modles
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    [JsonObject]
+    public class OdataModel<T>
+    {
+        [JsonProperty("@odata.context")]
+        public string OdataContext { get; set; }
+
+        [JsonProperty("value")]
+        public List<T> Values { get; set; }
+    }
+}
